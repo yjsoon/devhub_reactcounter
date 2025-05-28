9b902bcf6928592990748594071ab77dd0c53865
YJ Soon <yjsoon@tinkertanker.com>
2025-05-28 15:05:28 +0800

    Working counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // array destructuring
+  const [counter, setCounter] = useState(0);
+
+  function increase() {
+    setCounter(counter + 1);
+  }
+
+  function decrease() {
+    setCounter(counter - 1);
+  }
+
+  function reset() {
+    setCounter(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={increase}>Increment</button>
+      <button onClick={decrease}>Decrement</button>
+      <br />
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }

764cd72536223bc08133507bcf25fcf54aaac439
YJ Soon <yjsoon@tinkertanker.com>
2025-05-28 15:27:21 +0800

    Updated with conditional rendering and dynamic CSS

--- src/App.js
+++ src/App.js
@@ -19,11 +19,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{counter}</h1>
       <button onClick={increase}>Increment</button>
       <button onClick={decrease}>Decrement</button>
       <br />
       <button onClick={reset}>Reset</button>
+      <h1
+        className={`counter-${counter > 0 ? "positive" : "negative"}`}
+        style={{ fontSize: 24 + counter * 5, marginTop: counter * 10 }}>
+        {counter}
+      </h1>
+      <div className="message">
+        {counter === 0 && <p>Let's go!!!!!</p>}
+        {counter > 0 && counter <= 5 && <p>Just getting started...</p>}
+      </div>
     </div>
   );
 }
